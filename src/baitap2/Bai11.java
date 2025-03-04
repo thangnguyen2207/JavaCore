e95b02ccf5dbc3884a1b572fa159d7131319a6d5
Admin <Admin@DESKTOP-84OFD77>
2025-03-04 17:48:34 +0700

    commit 04/03

--- src/baitap2/Bai11.java
+++ src/baitap2/Bai11.java
@@ -10,46 +10,57 @@ public class Bai11 {
 	public static void main(String[] args) {
 		Arrays.sort(c);
 		System.out.println("Các phần tử trong mảng:\n" + Arrays.toString(c) + "\n");
-		remove("9".charAt(0));
-		System.out.println("Các phần tử trong mảng:\n" + Arrays.toString(c) + "\n");
 		
-//		try (Scanner scan = new Scanner(System.in)) {
-//			int option = 0;
-//			do {
-//				System.out.println("Chọn hành động:\n1. Tìm kiếm\n2. Thêm\n3. Xóa");
-//				option = scan.nextInt();
-//				
-//				switch (option) {
-//					case 1: {
-//						System.out.println("Nhập 1 ký tự cần tìm kiếm:");
-//						char key = scan.next().charAt(0);
-//						
-//						int timkiem = timkiem(key);
-//						if (timkiem == -1)
-//							System.out.println("Không tìm thấy ký tự " + key + " trong mảng");
-//						else
-//							System.out.println("Ký tự \"" + key + "\" nằm ở vị trí thứ " + timkiem + " trong mảng\n");
-//						
-//						break;
-//					}
-//					case 2: {
-//						System.out.println("Nhập 1 ký tự cần thêm:");
-//						char key = scan.next().charAt(0);
-//						
-//						if (add(key)) {
-//							System.out.println("\nĐã thêm ký tự \"" + key +"\" vào mảng\n");
-//							System.out.println("Các phần tử trong mảng:\n" + Arrays.toString(c) + "\n");
-//						};
-//						
-//						break;
-//					}
-//					default: {
-//						System.out.println("Kết thúc");
-//						break;
-//					}
-//				}
-//			} while (option > 0 && option < 4);
-//		}
+		try (Scanner scan = new Scanner(System.in)) {
+			int option = 0;
+			do {
+				System.out.println("Chọn hành động:\n1. Tìm kiếm\n2. Thêm\n3. Xóa");
+				option = scan.nextInt();
+				
+				switch (option) {
+					case 1: {
+						System.out.println("Nhập 1 ký tự cần tìm kiếm:");
+						char key = scan.next().charAt(0);
+						
+						int timkiem = timkiem(key);
+						if (timkiem == -1)
+							System.out.println("Không tìm thấy ký tự " + key + " trong mảng");
+						else
+							System.out.println("Ký tự \"" + key + "\" nằm ở vị trí thứ " + timkiem + " trong mảng\n");
+						
+						break;
+					}
+					case 2: {
+						System.out.println("Nhập 1 ký tự cần thêm:");
+						char key = scan.next().charAt(0);
+						
+						if (add(key)) {
+							System.out.println("\nĐã thêm ký tự \"" + key +"\" vào mảng\n");
+							System.out.println("Các phần tử trong mảng:\n" + Arrays.toString(c) + "\n");
+						} else
+							System.out.println("Không thể thêm ký tự vào mảng\n");;
+						
+						break;
+					}
+					case 3: {
+						System.out.println("Nhập 1 ký tự cần xóa:");
+						char key = scan.next().charAt(0);
+						
+						if (remove(key)) {
+							System.out.println("\nĐã xóa ký tự \"" + key +"\" khỏi mảng\n");
+							System.out.println("Các phần tử trong mảng:\n" + Arrays.toString(c) + "\n");
+						} else
+							System.out.println("Không thấy ký tự cần xóa\n");
+						
+						break;
+					}
+					default: {
+						System.out.println("Kết thúc");
+						break;
+					}
+				}
+			} while (option > 0 && option < 4);
+		}
 	}
 	
 	//Tìm kiếm ký tự và trả về index, -1 nếu không tìm thấy. Sử dụng vòng lặp for
